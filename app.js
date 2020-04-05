25d1e57f5a620d1a7257e699ec86352cf0dee939
duhbhavesh <connect-bhavesh-kasturi@gmail.com>
2020-04-05 18:45:20 +0530

    port and process.env

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 // PORT: Localhost port 3000
-var port = 3000 || process.env.PORT;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log("Server is listening");
 });
