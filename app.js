b4c2bd90f21bb2a61f3c5c893938d7f3d683a281
duhbhavesh <connect-bhavesh-kasturi@gmail.com>
2020-04-08 17:10:58 +0530

    app.js change for github

--- app.js
+++ app.js
@@ -26,32 +26,8 @@ var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12"
             useFindAndModify: false,
             useUnifiedTopology: true
         });
- 
 
-// mongoose.connect(process.env.DATABASEURL);
-// mongoose.connect("mongodb://localhost/yelp_camp_v12", {
-//         useNewUrlParser: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false,
-//         useUnifiedTopology: true
-//     }).then(() => {
-//         console.log('Connected to DB!');
-//     }).catch(err => {
-//         console.log('ERROR:', err.message);
-//     });
-
-// mongoose.connect("mongodb+srv://yelpcamp:403Error@cluster0-qj0fi.mongodb.net/test?retryWrites=true&w=majority", {
-//         useNewUrlParser: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false,
-//         useUnifiedTopology: true
-//     }).then(() => {
-//         console.log('Connected to DB!');
-//     }).catch(err => {
-//         console.log('ERROR:', err.message);
-//     });
-
-    
+        
 app.use(bodyParser.urlencoded({extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -71,20 +47,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// app.use(async function(req, res, next){
-//     res.locals.currentUser = req.user;
-//     if(req.user) {
-//      try {
-//        let user = await User.findById(req.user._id).populate('notifications', null, { isRead: false }).exec();
-//        res.locals.notifications = user.notifications.reverse();
-//      } catch(err) {
-//        console.log(err.message);
-//      }
-//     }
-//     res.locals.error = req.flash("error");
-//     res.locals.success = req.flash("success");
-//     next();
-//  });
+
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
